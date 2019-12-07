336e5cc49b54fa1ed0325be3f7be2a7a31771772
MarAllencar <55607165+MarAllencar@users.noreply.github.com>
2019-12-06 22:16:49 -0300

    Update satanas.js

--- satanas.js
+++ satanas.js
@@ -5,7 +5,7 @@ Nome: Mariane Alves de Alencar */
 
 
 
-private void btnEspaço_Click(object sender, EventArgs e)
+private void btnEspaço_Click(object sender, EventArgs)
 {
     double nb1;    /*banana = nb1*/ 
     double nb2;     /* choppe = nb2*/
